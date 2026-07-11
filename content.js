77124a2f9a33b88c079327d3de0336daacf17c36
Nashath Hasan <mdhnashath@gmail.com>
2026-07-11 13:05:59 +0530

    single user auto login

---
+++ content.js
@@ -0,0 +1,12 @@
+let usn=document.getElementById("username")
+usn.value="4MT23CS100"
+
+let date = document.getElementById("dd")
+date.value="12 "
+
+let month = document.getElementById("mm")
+month.value="11"
+
+let year = document.getElementById("yyyy")
+year.value="2005"
+
