25231c92117bc3599303666bd762d059720067cf
Nashath Hasan <mdhnashath@gmail.com>
2026-07-23 11:46:59 +0530

    allow site subdomains

--- content.js
+++ content.js
@@ -1,40 +1,7 @@
-<<<<<<< HEAD
-const hostname = window.location.hostname;
-
-if (!hostname.startsWith("mite-students.")) {
-    return;
-}
-=======
-
-/* check for the login button pressed
-
-let button = document.querySelector(".cn-submit1")
-
-button.addEventListener("click", function(){
-
-    let usn   = document.getElementById("username").value
-    let date  = document.getElementById("dd").value
-    let month = document.getElementById("mm").value
-    let year  = document.getElementById("yyyy").value
-
-    console.log("button clicked")
-
-    chrome.storage.local.set(
-        {
-         USN: usn,
-         DATE: date,
-         MONTH: month,
-         YEAR: year,
-         pending: true
-        })
-}
-)*/
-
 
 const url = window.location.hostname.split(".");
 
 if (url.startsWith("mite")) {
->>>>>>> e2994c2 (Permision granted to all pages)
 
 let usn=document.getElementById("username")
 usn.value="4MT23CS100"
